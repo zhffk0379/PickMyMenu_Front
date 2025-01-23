7c86b55c0ab248117a6e24c41e6e328097799ca8
nowcow <147683241+Cnowcow@users.noreply.github.com>
2025-01-23 13:11:49 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext'; // AuthContext 임포트
 import { HomePage, MapPage } from './routes';
 import { Join, Login, MyPage, PasswordVerify, Edit, Delete } from './components/member';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from "./components/layout/Layout";
 import SurveyResult from "./components/survey/SurveyResult";
@@ -12,7 +11,6 @@ import Ranking from "./routes/Ranking";
 import ChoicePage from "./routes/ChoicePage";
 import Restaurant from "./components/restaurant/Restaurant";
 import RestaurantInfo from "./components/restaurant/RestaurantInfo";
-import Review from "./components/review/Review";
 import MyRestaurantInfo from "./components/member/MyRestaurantInfo";
 import MyInfo from "./components/member/MyInfo";
 
@@ -24,10 +22,12 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} /> {/* 메인 페이지 */}
             <Route path="/map" element={<MapPage />} /> {/* 지도 페이지 */}
-
+            <Route path="/map" element={<MapPage />} /> {/* 지도 페이지 */}
+            <Route path="/myRestaurantInfo" element={<MyRestaurantInfo />} /> {/* 지도 페이지 */}
             <Route path="/join" element={<Join />} />
             <Route path="/login" element={<Login />} />
             <Route path="/mypage" element={<MyPage />} />
+            <Route path="/myinfo" element={<MyInfo />} />
             <Route path="/edit" element={<Edit />} />
             <Route path="/passwordverify" element={<PasswordVerify />} />
             <Route path="/choice" element={<ChoicePage />} />
